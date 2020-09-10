b7ee21331e70af15cc32edda7851161cc3638aa3
justsomeguy <thenaders3@gmail.com>
2020-09-09 19:59:59 -0700

    Updates

---
+++ precache-manifest.98ef18fddeb86ee9acd9d0c3ef60aa9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e3fde68e7dfbbb1e373a1e7ceace42",
+    "url": "/dapp-pratice/index.html"
+  },
+  {
+    "revision": "323b104b979d2aa53797",
+    "url": "/dapp-pratice/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5f608d89255e43b18c33",
+    "url": "/dapp-pratice/static/css/main.fbab8454.chunk.css"
+  },
+  {
+    "revision": "323b104b979d2aa53797",
+    "url": "/dapp-pratice/static/js/2.1ac2fded.chunk.js"
+  },
+  {
+    "revision": "9870115ff28e3add0b491ab406a81797",
+    "url": "/dapp-pratice/static/js/2.1ac2fded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f608d89255e43b18c33",
+    "url": "/dapp-pratice/static/js/main.9a8cb50e.chunk.js"
+  },
+  {
+    "revision": "d9093764635d6a6bbe3b",
+    "url": "/dapp-pratice/static/js/runtime-main.5ad4dd33.js"
+  }
+]);
