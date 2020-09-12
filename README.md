b269d350219de359d7404b3e897e86fdcc236588
justsomeguy <thenaders3@gmail.com>
2020-09-12 10:42:47 -0700

    updated deploy script and readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Brushing to make sure I don't get rusty :)
+Practice Dapp that integrates in [EIP 2612](https://github.com/ethereum/EIPs/blob/8a34d644aacf0f9f8f00815307fd7dd5da07655f/EIPS/eip-2612.md) into a test token on Kovan.
+
+BleepTokenContract Kovan Address: 0xf7189e50cc020658e0f4E17E6eED37470A333c75
+TestDepositContract Kovan Address: 0x6687BA38B7fBdfe62FAfB3f30FBA5219C6c6CEAC
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 const BleepTokenContract = artifacts.require("BleepTokenContract");
+const TestDepositContract = artifacts.require("TestDepositContract");
 
 module.exports = async function(deployer) {
-  await deployer.deploy(BleepTokenContract(20000, 'Bleep', 10, 'BLP'));
+  await deployer.deploy(BleepTokenContract);
+  const bleepTokenAddress = await BleepTokenContract.address;
+  await deployer.deploy(TestDepositContract, bleepTokenAddress);
 };
